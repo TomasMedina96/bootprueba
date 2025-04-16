b5b8dfcb1e24a05e0f0a4bae171a5ad6c6374d12
Tomas Medina <tomasmedina962@gmail.com>
2025-04-16 13:17:06 -0600

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,11 @@ function App() {
 
     return (
         <>
+        <h1>Medina Team</h1>
+        <p>Chat boot prueba</p> 
+        <p>Tel: +52 999 100 5143</p>
+        <p>Email: juanpablomedina020.193@gmail.com</p>
+        <p>Ubicacion: Merida, Yucatan</p>
             <Chatbot />
         </>
     );
